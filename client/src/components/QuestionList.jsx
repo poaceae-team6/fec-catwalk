016f813e0c814c4f8e7839f53bcff43317929436
Ximing Chen <steven.xm824@gmail.com>
2021-10-28 15:45:36 -0700

    add question list file

---
+++ client/src/components/QuestionList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class QuestionList extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render () {
+    return (
+      <div>
+        QuestionList goes here
+      </div>
+    )
+  }
+}
+
+export default QuestionList
