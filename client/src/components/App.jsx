c8fd3772a69ac5d5bae319c67678860bec85cc6c
Ximing Chen <steven.xm824@gmail.com>
2021-10-28 15:04:46 -0700

    set up server and App.jsx

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+
+    return (
+      <div id='app'>
+        <h1> react is running </h1>
+      </div>
+    )
+  }
+}
+
+export default App;
