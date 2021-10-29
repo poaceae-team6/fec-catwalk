6ecd45325a6cf7acacf1f34ec435376f6db45b9f
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-10-29 13:57:48 -0700

    Updated App Component - imported sampleData and placeholders for states that will be shared with OVerview. Added RelatedProducts Component in the render.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import QuestionList from './QuestionList.jsx';
 
+// Import sampleData for testing purposes
+// Related Products
+import sampleProductsData from '../../related_products/sampleProductsData.js';
+import sampleProductIdData from '../../related_products/sampleProductIdData.js'
+import sampleProductIdRelatedData from '../../related_products/sampleProductIdRelatedData.js';
+
 class App extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      // sample data for current product retrieved by id
+      tempCurrentProduct: {sampleProductIdData};
+      // shared component for Overview & Outfit
+      tempOutfitList: []
+    };
   }
 
   render () {
