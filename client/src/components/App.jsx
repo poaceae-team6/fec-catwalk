d6af4bd722cf40b5221263710702b15488c0907b
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-10-29 12:55:36 -0700

    Created React components for the Related Products section and an assets folder inside src with another folder called related_products for all the placeholder images/sampleData for my components.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,13 +9,8 @@ class App extends React.Component {
   render () {
 
     return (
-      <div id='app'>
-        <h1> react is running </h1>
-        <div>Overview Goes Here</div>
-        <div>Related items Goes Here</div>
-        <QuestionList />
-        <div>Review Goes Here</div>
-      </div>
+      <RelatedProducts />
+      <QuestionList />
     )
   }
 }
