e4ea77c6dcc0adbe3e479a104ecf0af207eaaa32
Alice Li <oceanylee@gmail.com>
2021-10-29 23:42:33 -0700

    Rewrited all components in Function style

--- client/src/components/review/reviewmain/RatingBreakdown.jsx
+++ client/src/components/review/reviewmain/RatingBreakdown.jsx
@@ -3,21 +3,14 @@ import RatingSummary from './RatingSummary.jsx';
 import Recommend from './Recommend.jsx';
 import StarBreakdown from './StarBreakdown.jsx';
 
-
-class RatingBreakdown extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render () {
-    return (
-      <div>
-        <div><RatingSummary /></div>
-        <div><Recommend /></div>
-        <div><StarBreakdown /></div>
-      </div>
-    )
-  }
+function RatingBreakdown() {
+  return (
+    <div>
+      <div><RatingSummary /></div>
+      <div><Recommend /></div>
+      <div><StarBreakdown /></div>
+    </div>
+  );
 }
 
 
--- client/src/components/review/reviewmain/ReviewMain.jsx
+++ client/src/components/review/reviewmain/ReviewMain.jsx
@@ -3,25 +3,17 @@ import RatingBreakdown from './RatingBreakdown.jsx'
 import ProductBreakdown from './ProductBreakdown.jsx'
 import Reviews from './Reviews.jsx'
 
-
-class ReviewMain extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render () {
-
-    return (
-      <div>
-        <h2>REVIEW</h2>
-        <div id='leftbar'>
-          <div><RatingBreakdown /></div>
-          <div><ProductBreakdown /></div>
-        </div>
-        <div id='rightbar'><Reviews /></div>
+function ReviewMain() {
+  return (
+    <div>
+      <h2>REVIEW</h2>
+      <div id='leftbar'>
+        <div><RatingBreakdown /></div>
+        <div><ProductBreakdown /></div>
       </div>
-    )
-  }
+      <div id='rightbar'><Reviews /></div>
+    </div>
+  );
 }
 
 
