fa161a9aaf61fbfeb0e45beabfe19f77c2a82bf2
Alice Li <oceanylee@gmail.com>
2021-10-30 23:12:44 -0700

    Completed the StarRating component

--- client/src/components/review/reviewmain/StarRating.jsx
+++ client/src/components/review/reviewmain/StarRating.jsx
@@ -1,29 +1,49 @@
 import React, { useState } from "react";
-import styled from 'styled-components';
+import { FaStar } from "react-icons/fa";
 
 
 const StarRating = () => {
-  const [rating, setRating] = useState(0);
-  const [hover, setHover] = useState(0);
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+
+  const starInputStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '1px',
+    height: '1px',
+    clip: 'rect(1px, 1px, 1px, 1px)'
+  }
+
   return (
-    <div className="star-rating">
-      {[...Array(5)].map((star, index) => {
-        index += 1;
+
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+
         return (
-          <button
-            type="button"
-            key={index}
-            className={index <= (hover || rating) ? "on" : "off"}
-            onClick={() => setRating(index)}
-            onMouseEnter={() => setHover(index)}
-            onMouseLeave={() => setHover(rating)}
-          >
-            <span className="star">&#9733;</span>
-          </button>
+          <label key={i}>
+            <input
+              type= "radio"
+              name= "rating"
+              style = {starInputStyle}
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+              onMouseOver={() => setHover(ratingValue)}
+              onMouseOut={() => setHover(null)}
+            />
+            <FaStar
+              className="star"
+              color= {ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+              size= {20}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+            />
+          </label>
         );
       })}
     </div>
-  );
+  )
 };
 
 export default StarRating;
