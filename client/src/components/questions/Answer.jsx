4c16d11fc0c21de6dd28003731f9e3d52a3a1101
Ximing Chen <steven.xm824@gmail.com>
2021-11-01 15:15:29 -0700

    add moment

--- client/src/components/questions/Answer.jsx
+++ client/src/components/questions/Answer.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import moment from 'moment';
 
 const Answer = (props) => {
 
+  //format the date
+  let date = moment(props.answer.date).format('LL');
 
   if (props.answer.photos.length === 0) {
 
     return (
       <div>
         <p> A: {props.answer.body}</p>
-        <span> by User {props.answer.answerer_name}</span>  |  <span>helpful?  Yes ({props.answer.helpfulness})</span>  |  <span>report</span>
+        <span> by User {props.answer.answerer_name}, {date}</span>  |  <span>helpful?  Yes ({props.answer.helpfulness})</span>  |  <span>report</span>
       </div>
 
     )
@@ -18,7 +21,7 @@ const Answer = (props) => {
       <div>
         <p> A: {props.answer.body}</p>
         {props.answer.photos.map((img, index) => <img key={index} src={img} />)}
-        <span> by User {props.answer.answerer_name}</span>  |  <span>helpful?  Yes ({props.answer.helpfulness})</span>  |  <span>report</span>
+        <span> by User {props.answer.answerer_name}, {date}</span>  |  <span>helpful?  Yes ({props.answer.helpfulness})</span>  |  <span>report</span>
       </div>
 
     )
