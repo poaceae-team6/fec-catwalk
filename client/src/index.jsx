d119882b20daf80110920f3436eff948f2423dba
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-11-02 00:03:35 -0700

    nothing changed.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,12 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-import { ThemeProvider } from './components/ThemeContext.js';
 
-
-ReactDOM.render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
