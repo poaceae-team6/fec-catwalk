13a05b6beede93290d9330095269c45280690b87
Ximing Chen <steven.xm824@gmail.com>
2021-11-01 19:12:17 -0700

    display seller's username in bold

--- client/src/components/questions/Answer.jsx
+++ client/src/components/questions/Answer.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 
 const Answer = (props) => {
 
+
   //format the date
   let date = moment(props.answer.date).format('LL');
+
+  let bold = {
+   fontWeight: 'normal'
+  };
+
+  if(props.answer.answerer_name === 'Seller') {
+    console.log('I am seller');
+    bold.fontWeight = 'bold';
+  }
+
 
   if (props.answer.photos.length === 0) {
 
     return (
       <div>
         <p> A: {props.answer.body}</p>
-        <span> by User {props.answer.answerer_name}, {date}</span>  |  <span>helpful?  Yes ({props.answer.helpfulness})</span>  |  <span>report</span>
+        <span> by User</span> <span style={bold}> {props.answer.answerer_name}</span><span>, {date}</span>  |  <span>helpful?  Yes ({props.answer.helpfulness})</span>  |  <span>report</span>
       </div>
 
     )
@@ -21,7 +32,7 @@ const Answer = (props) => {
       <div>
         <p> A: {props.answer.body}</p>
         {props.answer.photos.map((img, index) => <img key={index} src={img} />)}
-        <span> by User {props.answer.answerer_name}, {date}</span>  |  <span>helpful?  Yes ({props.answer.helpfulness})</span>  |  <span>report</span>
+        <span style={bold}> by User {props.answer.answerer_name}, {date}</span>  |  <span>helpful?  Yes ({props.answer.helpfulness})</span>  |  <span>report</span>
       </div>
 
     )
