e550c1cf2964e1d1647ce33e729ecd3c7964efa5
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-11-03 00:21:29 -0700

    Added OverviewStyle component - finished the steps in planning.md.

---
+++ client/src/components/overview/OverviewStyle.jsx
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+
+  // product object (category, name, description, 'features')
+  // 'styles' object.results[0] (image url - "photos[0].thumbnail_url" and price - "original_price", "sale_price")
+
+const OverviewStyle = (props) => {
+  
+  const handleClick = () => {
+    props.setCurrentStyleIndex(props.key);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick.bind(this)}>
+        <img className='style-img' value={props.currentStyle} src={props.currentStyle.photos[0].thumbnail_url}/>
+      </button>
+    </div>
+  )
+};
+
+export default OverviewStyle;
