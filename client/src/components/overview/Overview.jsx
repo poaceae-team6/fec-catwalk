22d9102874d4470be8ad6678e58eefd7b798cbe4
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-11-04 10:00:54 -0700

    Added the css grid system.

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -5,33 +5,33 @@ import RelatedProducts from '../related_products/RelatedProducts.jsx';
 import YourOutfit from '../related_products/YourOutfit.jsx';
 import OverviewStyle from './OverviewStyle.jsx';
 
+import {IoMdHeartEmpty} from 'react-icons/io';
+import {IoMdHeart} from 'react-icons/io';
+
 var outfitList = [];
   // product object (category, name, description, 'features')
   // 'styles' object.results[0] (image url - "photos[0].thumbnail_url" and price - "original_price", "sale_price")
-const url = 'http://127.0.0.1:3000';
+const url = 'http://localhost:3000';
 
 const Overview = (props) => {
 
   const [styles, setStyles] = useState(null);
-  const [currentStyleIndex, setCurrentStyleIndex] = useState(0);
   const [outfits, setOutfits] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isInOutfits, setIsInOutfit] = useState(false);
 
-  const fetchData = () => {
-    setIsLoading(true);
-    axios.get(`${url}/products/${props.currentProduct.id}/styles`)
+  useEffect(() => {
+    fetchData(props.currentProduct.id);
+  }, [])
+  
+  const fetchData = (productId) => {
+    axios.get(`${url}/products/${productId}/styles`)
     .then(res => {
       setStyles(res.data.results);
     })
     .catch(error => {
       console.log(error);
     })
-    setIsLoading(false);
   }
-
-  useEffect(() => {
-    fetchData();
-  }, [])
 
   const updateOutfitList = (copyArray) => {
     outfitList = copyArray;
@@ -43,13 +43,28 @@ const Overview = (props) => {
   } else {
     return (
       <div className='overview-container'>
-      <h2>{styles[currentStyleIndex].name}</h2>
-      {styles.map( (style, index) => {
-        return <OverviewStyle setCurrentStyleIndex={setCurrentStyleIndex.bind(this)} currentStyle={styles[currentStyleIndex]} key={index}/>
-      })}
-      <RelatedProducts currentProduct={props.currentProduct} fetchNewProduct={props.fetchNewProduct.bind(this)}/>
-      <YourOutfit currentProduct={props.currentProduct} currentStyle={styles[currentStyleIndex]}/>
-    </div>
+        <div className='overview-grid'>
+          <h2>{props.currentProduct.name}</h2>
+          <div className='gallery'>
+            <img src={styles[0].photos[0].thumbnail_url}/>
+          </div>
+          <div className='overview-buttons'>
+            <button id='cart-btn'>
+              <h3>ADD TO BAG</h3>
+            </button>
+            <button id='heart-icon'>
+              {isInOutfits ? <IoMdHeart /> : <IoMdHeartEmpty />}
+            </button>
+          </div>
+          <div className='styles'>
+            {styles.map( (style, index) => {
+              return <OverviewStyle currentStyle={styles[index]} key={index}/>
+            })}
+          </div>
+        </div>
+        <RelatedProducts currentProduct={props.currentProduct} fetchNewProduct={props.fetchNewProduct.bind(this)}/>
+        <YourOutfit currentProduct={props.currentProduct} />
+      </div>
     )
   }
   // return content;
