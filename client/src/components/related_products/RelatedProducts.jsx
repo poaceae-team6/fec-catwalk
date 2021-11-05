7a1544129a42393fa63763c553872e2ba543bcd3
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-11-05 12:43:14 -0700

    Updated the arrow into react-icons and split the onClick function into 2 so that it can be attached to the icon as well as the button tag.

--- client/src/components/related_products/RelatedProducts.jsx
+++ client/src/components/related_products/RelatedProducts.jsx
@@ -3,7 +3,8 @@ import RelatedProductsItem from './RelatedProductsItem.jsx';
 import { ThemeContext } from '../ThemeContext.js';
 
 import slide from '../../../dist/css_animations/horizontalScroll.js';
-import sampleProductsData from '../../assets/related_products/sampleProductsData.js';
+import { MdArrowForwardIos } from 'react-icons/md';
+import { MdArrowBackIos } from 'react-icons/md';
 import axios from 'axios';
 
 const url = 'http://127.0.0.1:3000';
@@ -26,12 +27,12 @@ const RelatedProducts = (props) => {
       })
   }
   
-  const handleClick = (e) => {
-    if (e.target.id === 'left-arrow') {
-      slide('products-slide', 'left', 1000, 500);
-    } else {
-      slide('products-slide', 'right', 1000, 500);
-    }
+  const handleLeftArrow = () => {
+    slide('products-slide', 'left', 1000, 500);
+  }
+  
+  const handleRightArrow = () => {
+    slide('products-slide', 'right', 1000, 500);
   }
   
   if (relatedProducts === null) {
@@ -40,16 +41,22 @@ const RelatedProducts = (props) => {
     return (  
       <ThemeContext.Consumer>
         {darkMode => (
+          // className={darkMode ? 'arrow-dark left' : 'arrow left'}
           <div className='products-container'>
             <h2 className='products-list-title'>RELATED PRODUCTS</h2>
-            <button onClick={handleClick.bind(this)} id='left-arrow' className={darkMode ? 'arrow-dark left' : 'arrow left'}/>
-            <div className='horizontal-slide' id='products-slide'>
-              {relatedProducts.map( (productId, index) => {
-                // should pass 2 objects - product & styles for specific id
-                return <RelatedProductsItem productId={productId} key={index}/>
-              })}
+            <div className='scroll-container'>
+              <button className='arrow' onClick={handleLeftArrow.bind(this)}>
+                <MdArrowBackIos onClick={handleLeftArrow.bind(this)}/>
+              </button>
+              <div className='horizontal-slide' id='products-slide'>
+                {relatedProducts.map( (productId, index) => {
+                  return <RelatedProductsItem productId={productId} key={index}/>
+                })}
+              </div>
+              <button className='arrow' onClick={handleRightArrow.bind(this)}>
+                <MdArrowForwardIos onClick={handleRightArrow.bind(this)}/>
+              </button> 
             </div>
-            <button onClick={handleClick.bind(this)} id='right-arrow' className={darkMode ? 'arrow-dark right' : 'arrow right'}/>
           </div>
         )}
       </ThemeContext.Consumer>
