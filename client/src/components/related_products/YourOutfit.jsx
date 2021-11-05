34ae4deb19d4e53c4382c77b795d83d545fa8b6a
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-11-05 12:44:10 -0700

    Updated the horizontal scroll changes and render now only does the conditional change INSIDE the card area.

--- client/src/components/related_products/YourOutfit.jsx
+++ client/src/components/related_products/YourOutfit.jsx
@@ -3,17 +3,19 @@ import YourOutfitItem from './YourOutfitItem.jsx';
 import YourOutfitItemDefault from './YourOutfitItemDefault.jsx';
 import { ThemeContext } from '../ThemeContext.js';
 
+import { MdArrowForwardIos } from 'react-icons/md';
+import { MdArrowBackIos } from 'react-icons/md';
+
 import slide from '../../../dist/css_animations/horizontalScroll.js';
-import sampleProductsData from '../../assets/related_products/sampleProductsData.js';
 
 const YourOutfit = (props) => {
   
-  const handleClick = (e) => {
-    if (e.target.id === 'left-arrow') {
-      slide('outfit-slide', 'left', 1000, 500);
-    } else {
-      slide('outfit-slide', 'right', 1000, 500);
-    }
+  const handleLeftArrow = () => {
+    slide('outfit-slide', 'left', 1000, 500);
+  }
+  
+  const handleRightArrow = () => {
+    slide('outfit-slide', 'right', 1000, 500);
   }
   
   // fetch styles data using id to get the image url
@@ -21,40 +23,29 @@ const YourOutfit = (props) => {
   // 2. Use that id to fetch relatedProducts array of id's
   // 3. Iterate over each id and fetch for that specific product object (category, name, description, 'features') & 'styles' object.results[0] (image url - "photos[0].thumbnail_url" and price - "original_price", "sale_price")
   
-  if (props.outfits.length < 1) {
-    return (
-      <ThemeContext.Consumer>
-        {darkMode => (
-          <div className='outfit-container'>
-            <h2 className='products-list-title'>YOUR OUTFIT</h2>
-            <button onClick={handleClick.bind(this)} id='left-arrow' className={darkMode ? 'arrow-dark left' : 'arrow left'}/>
+  return (  
+    <ThemeContext.Consumer>
+      {darkMode => (
+        // className={darkMode ? 'arrow-dark left' : 'arrow left'}
+        <div className='outfit-container'>
+          <h2 className='products-list-title'>YOUR OUTFIT</h2>
+          <div className='scroll-container'>
+            <button className='arrow' onClick={handleLeftArrow.bind(this)}>
+              <MdArrowBackIos onClick={handleLeftArrow.bind(this)}/>
+            </button>
             <div className='horizontal-slide' id='outfit-slide'>
-              <YourOutfitItemDefault />
+              {props.outfits.length ? relatedProducts.map( (productId, index) => {
+                return <RelatedProductsItem productId={productId} key={index}/>
+              }) : <YourOutfitItemDefault />}
             </div>
-            <button onClick={handleClick.bind(this)} id='right-arrow' className={darkMode ? 'arrow-dark right' : 'arrow right'}/>
+            <button className='arrow' onClick={handleRightArrow.bind(this)}>
+              <MdArrowForwardIos onClick={handleRightArrow.bind(this)}/>
+            </button> 
           </div>
-        )}
-      </ThemeContext.Consumer>
-    )
-  } else {
-    return (  
-      <ThemeContext.Consumer>
-        {darkMode => (
-          <div className='outfit-container'>
-            <h2 className='products-list-title'>YOUR OUTFIT</h2>
-            <button onClick={handleClick.bind(this)} id='left-arrow' className={darkMode ? 'arrow-dark left' : 'arrow left'}/>
-            <div className='horizontal-slide' id='outfit-slide'>
-              {sampleProductsData.map(item => {
-                // should pass 2 objects - product & styles for specific id
-                return <YourOutfitItem itemData={item} itemStyle={item} key={item.id}/>
-              })}
-            </div>
-            <button onClick={handleClick.bind(this)} id='right-arrow' className={darkMode ? 'arrow-dark right' : 'arrow right'}/>
-          </div>
-        )}
-      </ThemeContext.Consumer>
-    );
-  }
+        </div>
+      )}
+    </ThemeContext.Consumer>
+  );
 }
 
 export default YourOutfit;
