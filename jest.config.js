84b309914e8cbeab2783f30f878f30138bde98b2
Alice Li <oceanylee@gmail.com>
2021-11-08 01:22:43 -0800

    Add test case for reviews API

--- jest.config.js
+++ jest.config.js
@@ -137,7 +137,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -158,6 +158,7 @@ module.exports = {
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
+  testRegex: "((\\.|/*.)(spec))\\.js?$",
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
@@ -184,7 +185,7 @@ module.exports = {
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
+  verbose: true,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
