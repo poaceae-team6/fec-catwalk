9814da3c63f4911b08eb41179a8b8b3b6bf666b7
Ximing Chen <steven.xm824@gmail.com>
2021-11-11 10:34:46 -0800

    CSS change

--- client/src/components/questions/AddQuestionModal.jsx
+++ client/src/components/questions/AddQuestionModal.jsx
@@ -5,7 +5,7 @@ import { ThemeContext } from '../ThemeContext.js';
 
 const AddQuestionModal = (props) => {
 
-  const [state, setState] = useState ({
+  const [state, setState] = useState({
     name: '',
     email: '',
     question: '',
@@ -14,11 +14,6 @@ const AddQuestionModal = (props) => {
   });
 
   const productId = props.id;
-
-  // eslint-disable-next-line no-unused-vars
-  const refresh = () => {
-    props.getData();
-  };
 
   // handle input changes
 
@@ -52,7 +47,7 @@ const AddQuestionModal = (props) => {
     let result = false;
 
     if (property === 'email') {
-      if(state[property].indexOf('@') !== -1 && state[property].length > 5 && state[property].indexOf('.com') !== -1) {
+      if (state[property].indexOf('@') !== -1 && state[property].length > 5 && state[property].indexOf('.com') !== -1) {
         result = true;
       }
     } else if (state[property].length !== 0) {
@@ -73,7 +68,7 @@ const AddQuestionModal = (props) => {
     let error = [];
 
     if (!name) {
-     error.push('Name');
+      error.push('Name');
 
     }
     if (!email) {
@@ -84,16 +79,7 @@ const AddQuestionModal = (props) => {
     }
 
     if (name && email && question) {
-      //submit ok add question
-      // let questionData = {
-      //   question_body: state.question,
-      //   asker_name: state.name,
-      //   answers: [],
-      //   question_helpfulness: 0,
-      //   reported: false
-      // };
 
-      // props.addQ(questionData);
       // send this data to API
       let postObj = {
         body: state.question,
@@ -103,17 +89,12 @@ const AddQuestionModal = (props) => {
       }
 
       axios.post(`/questions/${productId}`, postObj)
-      .then(res => {
-        console.log('add question ok', res);
-        props.getData();
-        props.close ();
-      })
-      .catch(err => console.log('add question err', err));
-
-      // props.getData()
-
-      // // close the window
-      // props.close ();
+        .then(res => {
+          console.log('add question ok', res);
+          props.getData();
+          props.close();
+        })
+        .catch(err => console.log('add question err', err));
 
     } else {
       setState({
@@ -129,35 +110,35 @@ const AddQuestionModal = (props) => {
     <ThemeContext.Consumer>
       {darkMode => (
         <div className='popup-modal' >
-        <div className={darkMode ? 'popup-inner-modal-dark' : 'popup-inner-modal'} >
-          <div>
-            <button className='close-btn' onClick={props.close}>X</button>
+          <div className={darkMode ? 'popup-inner-modal-dark' : 'popup-inner-modal'} >
+            <div>
+              <button className='close-btn' onClick={props.close}>X</button>
+            </div>
+            <div className='modal-title'>
+              <p>Ask Your Question</p>
+              <p className='modal-subtitle'>Question about {props.name}</p>
+            </div>
+            <div className='modal-text'>
+              <form onSubmit={handleSubmit}>
+                <p>What is your nickname? *</p>
+                <input onChange={handleNameInput} className='small-input' type='text' placeholder='Example: jackson11!' />
+                <p>For privacy reasons, do not use your full name or email address</p>
+                <br></br>
+                <p>Your email *</p>
+                <input onChange={handleEmailInput} className='small-input' type='text' placeholder='Why did you like the product or not?' />
+                <p>For authentication reasons, you will not be emailed</p>
+                <br></br>
+                <p>Your Question *</p>
+                <input className='large-input' onChange={handleQInput} type='text' placeholder='question body' />
+                <br></br>
+                {state.error && <Error msg={state.msg} />}
+                <br></br>
+                <input className='modal-btn' type='submit' value='submit' />
+                <input className='modal-btn' onClick={props.close} type='submit' value='cancel' />
+                <br></br>
+              </form>
+            </div>
           </div>
-        <div className='modal-title'>
-          <p>Ask Your Question</p>
-          <p className='modal-subtitle'>Question about {props.name}</p>
-        </div>
-        <div className='modal-text'>
-          <form onSubmit={handleSubmit}>
-            <p>What is your nickname? *</p>
-            <input onChange={handleNameInput} className='small-input' type='text' placeholder='Example: jackson11!'/>
-            <p>For privacy reasons, do not use your full name or email address</p>
-            <br></br>
-            <p>Your email *</p>
-            <input onChange={handleEmailInput} className='small-input' type='text' placeholder='Why did you like the product or not?'/>
-            <p>For authentication reasons, you will not be emailed</p>
-            <br></br>
-            <p>Your Question *</p>
-            <input className='large-input' onChange={handleQInput} type='text' placeholder='question body' />
-            <br></br>
-            {state.error && <Error msg={state.msg} />}
-            <br></br>
-            <input type='submit' value='submit'/>
-            <input onClick={props.close} type='submit' value='cancel'/>
-            <br></br>
-          </form>
-        </div>
-        </div>
         </div>
       )}
 
