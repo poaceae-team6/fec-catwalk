24a1c03c174d4ba234e42a19b1f0f27208148639
Alice Li <oceanylee@gmail.com>
2021-11-11 15:05:44 -0800

    Change the order of rating breakdown and enable text wrap in review list

--- client/src/components/review/reviewmain/ReviewLeft.jsx
+++ client/src/components/review/reviewmain/ReviewLeft.jsx
@@ -43,7 +43,7 @@ function ReviewLeft({ productId }) {
        res.data.recommended = Math.round(Number(res.data.recommended.true)/res.data.reveiwTotal * 100)
 
        // Calculate starBreakdown
-       res.data.ratingBreakdown = [Math.round(Number(res.data.ratings['1']|| '0') / res.data.reveiwTotal * 100), Math.round(Number(res.data.ratings['2']|| '0') / res.data.reveiwTotal * 100), Math.round(Number(res.data.ratings['3'|| '0']) / res.data.reveiwTotal * 100), Math.round(Number(res.data.ratings['4']|| '0') / res.data.reveiwTotal * 100), Math.round(Number(res.data.ratings['5']|| '0') / res.data.reveiwTotal * 100)]
+       res.data.ratingBreakdown = [Math.round(Number(res.data.ratings['5']|| '0') / res.data.reveiwTotal * 100), Math.round(Number(res.data.ratings['4']|| '0') / res.data.reveiwTotal * 100), Math.round(Number(res.data.ratings['3'|| '0']) / res.data.reveiwTotal * 100), Math.round(Number(res.data.ratings['2']|| '0') / res.data.reveiwTotal * 100), Math.round(Number(res.data.ratings['1']|| '0') / res.data.reveiwTotal * 100)]
 
       //  for(var key in res.data.characteristics) {
       //   res.data.char[key] = Number(res.data.characteristics.key.value).toFixed(2)
@@ -73,9 +73,9 @@ function ReviewLeft({ productId }) {
         {/* <ProgressBar ratingStar = '1' ratingBreakdown ={reviewContext.reviewMeta.ratingBreakdown[0]}/>
         {console.log('ratingBreakdown', reviewContext.reviewMeta.ratingBreakdown)} */}
         <ul style={{lineHeight: '2.3', padding: '0px'}}>
-          {reviewContext.reviewMeta.ratingBreakdown ? reviewContext.reviewMeta.ratingBreakdown.map((ratingBreakdown, i) =>
-            <ProgressBar ratingBreakdown={ratingBreakdown} key={i} ratingStar= {i + 1} />
-          ) : null}
+          <li>{reviewContext.reviewMeta.ratingBreakdown ? reviewContext.reviewMeta.ratingBreakdown.map((ratingBreakdown, i) =>
+            <ProgressBar ratingBreakdown={ratingBreakdown} key={i} ratingStar= {5 - i} />
+          ) : null}</li>
         </ul>
       </div>
       <br></br>
