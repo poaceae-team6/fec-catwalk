209fb5f668ba5dc03d688b9dd253c1557cf39500
Chiaoling Hong <chiaolinghong3d@gmail.com>
2021-11-11 10:20:38 -0800

    Updated StarRating.jsx with ThemeContext for more precise darkMode control.

--- client/src/components/review/reviewmain/StarRating.jsx
+++ client/src/components/review/reviewmain/StarRating.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { ThemeContext } from '../../ThemeContext.js';
 
 const StarRating = (props) => {
-
 
   const ratingWidthStyle = {
     width: `${props.rating / 5 * 100}%`
   }
-
+  
   return (
-    <div className="star-ratings" style={{width: '80px'}}>
-      <div className="fill-ratings" style={ ratingWidthStyle }>
-        <span>★★★★★</span>
-      </div>
-      <div className="empty-ratings">
-        <span>★★★★★</span>
-      </div>
-    </div>
+    <ThemeContext.Consumer>
+      {darkMode => (
+        <div className="star-ratings" style={darkMode ? {width: '80px', 'WebkitTextStroke': '1px #999', color: '#1f1e1e'} : {width: '80px'}}>
+          <div className="fill-ratings" style={darkMode ? { width: `${props.rating / 5 * 100}%`, color: 'white'} : { width: `${props.rating / 5 * 100}%`}}>
+            <span>★★★★★</span>
+          </div>
+          <div className="empty-ratings">
+            <span>★★★★★</span>
+          </div>
+        </div>
+      )}
+    </ThemeContext.Consumer>
   )
 };
 
